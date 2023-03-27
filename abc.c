ed19b939a6b26cbf1996c83b30c1902bb0c70a86
Priyanshi <priyanshikhataniya6@gmail.com>
2023-03-27 23:06:27 +0530

    Initial commit

---
+++ abc.c
@@ -0,0 +1,18 @@
+##include<stdio.h>
+int main(){
+  int m1,m2,m3;
+  printf("Enter marks of three subjects");
+  scanf("%d%d%d",&m1,&m2,&m3);
+  if(m1>=33&&m2>=33)
+  
+    printf("Price Rs. 45");
+      
+      
+      else if(m3>=33)
+      printf("Price 15 rupees");
+      else if(m2>=33)
+      printf("15");
+      else if(m1>=33)
+       printf("15");
+      return 0;
+}
